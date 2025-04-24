e996b3d85ff19e8d225d66aacd8cbb5511251c6f
Matthewlilol <101560931+Matthewlilol@users.noreply.github.com>
2025-04-25 00:42:02 +0800

    Add files via upload

---
+++ Test/songlist.js
@@ -0,0 +1,40 @@
+{
+  "songs": [
+    {
+      "name": "Ten Years",
+      "artist": "Eason Chan",
+      "genre": "love",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/249811396&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    },
+    {
+      "name": "Below Mount Fuji (Piano Version)",
+      "artist": "Eason Chan",
+      "genre": "love",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/611613633&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    },
+    {
+      "name": "Nocturne",
+      "artist": "Jay Chou",
+      "genre": "love",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/152935585&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    },
+    {
+      "name": "Maple Leaf",
+      "artist": "Jay Chou",
+      "genre": "love",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/23077688&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    },
+    {
+      "name": "April 7",
+      "artist": "Russel",
+      "genre" : "R&B, Electric",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2070574816&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    },
+    {
+      "name": "Fleeting Movements",
+      "artist": "Tawbe",
+      "genre": "Lo-Fi, Chillhop, Jazz",
+      "soundcloud_embed": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1935414902&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    }
+  ]
+}
